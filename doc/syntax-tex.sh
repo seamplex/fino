4b29c58f069f1e4712cdf9f542a1e5a52072c37b
Jeremy Theler <jeremy@seamplex.com>
2018-09-11 20:02:35 -0300

    latex highlighting

--- doc/syntax-tex.sh
+++ doc/syntax-tex.sh
@@ -1,23 +1,9 @@
 #!/bin/sh
-# 
-# $ ./syntax-tex.sh > ../syntax.tex
-# 
+#  Generate keywords.tex with
+#    $ ./syntax-tex.sh > keywords.tex
+#  and then include syntax.tex (which includes keywords.tex) in your preamble
 
 . ./keywords.sh
-
-cat << EOF
-\definecolor{was_fondo}{rgb}{0.95,0.95,0.90}
-\definecolor{was_keyword1}{rgb}{0.0,0.0,0.0}
-\definecolor{was_keyword2}{rgb}{0.0,0.2,0.0}
-\definecolor{was_variable}{rgb}{0.5,0.2,0.2}
-\definecolor{was_function}{rgb}{0.2,0.5,0.2}
-\definecolor{was_comment}{rgb}{0.5,0.5,0.5}
-
-\definecolor{bash_fondo}{rgb}{0.97,0.97,0.97}
-\definecolor{bash_keyword1}{rgb}{0.9,0.9,0.9}
-\definecolor{bash_keyword2}{rgb}{0.7,0.7,0.7}
-\definecolor{bash_comment}{rgb}{0.5,0.5,0.5}
-EOF
 
 # primary keywords
 echo "\lstdefinelanguage{fino}{"
@@ -51,54 +37,5 @@ cat << EOF
 sensitive=true,
 morecomment=[l]{\#},
 morestring=[b]\",
-}
-
-\newcommand{\MyHookSign}{\hbox{\ensuremath{\hookleftarrow}}}
-
-\lstset{
-  language=fino,
-  basicstyle=\footnotesize,
-  commentstyle={\color{was_comment}\textit},
-  keywordstyle=[1]{\color{was_keyword1}\ttfamily\textbf},
-  keywordstyle=[2]{\color{was_keyword2}\ttfamily\textbf},
-  keywordstyle=[3]{\color{was_variable}\textit},
-  keywordstyle=[4]{\color{was_function}\textbf},
-  backgroundcolor=\color{was_fondo},
-  breaklines=true,
-  prebreak={\space\MyHookSign},
-  xleftmargin=0.8cm,
-  xrightmargin=0.5cm,
-  frame=single,
-  framesep=0.5cm
-}
-
-
-\lstdefinestyle{fino}{
-  language=fino,
-  basicstyle=\footnotesize,
-  commentstyle={\color{was_comment}\textit},
-  keywordstyle=[1]{\color{was_keyword1}\ttfamily\textbf},
-  keywordstyle=[2]{\color{was_keyword2}\ttfamily\textbf},
-  keywordstyle=[3]{\color{was_variable}\textit},
-  keywordstyle=[4]{\color{was_function}\textbf},
-  backgroundcolor=\color{was_fondo},
-  breaklines=true,
-  prebreak={\space\MyHookSign},
-  xleftmargin=0.5cm,
-  xrightmargin=0.5cm,
-  framesep=0.5cm,
-  frame=single,
-}
-
-\lstdefinestyle{bash}{
-  language=,
-  basicstyle=\footnotesize\ttfamily,
-  backgroundcolor=\color{bash_fondo},
-  breaklines=true,
-  prebreak={\space\MyHookSign},
-  xleftmargin=0.2cm,
-  xrightmargin=0.2cm,
-  framesep=0.2cm,
-  frame=single,
 }
 EOF
