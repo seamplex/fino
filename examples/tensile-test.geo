84be0f59cb0267b980533e5bf6d36ceb41cfcd42
gtheler <jeremy@seamplex.com>
2018-10-20 19:20:58 -0300

    readme & tensile test

--- examples/tensile-test.geo
+++ examples/tensile-test.geo
@@ -4,6 +4,10 @@ a() = ShapeFromFile("tensile-test-specimen.step");
 
 Mesh.CharacteristicLengthMax = 3;
 Mesh.Algorithm = 6;
+Mesh.Algorithm3D = 10;
+Mesh.Optimize = 1;
+Mesh.OptimizeNetgen = 1;
+Mesh.HighOrderOptimize = 1;
 Mesh.ElementOrder = 2;
 
 Physical Surface("left") = {1};
--- examples/tensile-test.py
+++ examples/tensile-test.py
@@ -1,62 +1,116 @@
-try: paraview.simple
-except: from paraview.simple import *
+# state file generated using paraview version 5.1.2
+
+# ----------------------------------------------------------------
+# setup views used in the visualization
+# ----------------------------------------------------------------
+
+#### import the simple module from the paraview
+from paraview.simple import *
+#### disable automatic camera reset on 'Show'
 paraview.simple._DisableFirstRenderCameraReset()
 
-a1_sigma_PiecewiseFunction = CreatePiecewiseFunction( Points=[0.0, 0.0, 0.5, 0.0, 480.0, 1.0, 0.5, 0.0] )
+# Create a new 'Render View'
+renderView1 = CreateView('RenderView')
+renderView1.ViewSize = [1215, 822]
+renderView1.AxesGrid = 'GridAxes3DActor'
+renderView1.OrientationAxesVisibility = 0
+renderView1.StereoType = 0
+renderView1.CameraPosition = [119.85527680930403, 83.5687295595565, 215.494572652346]
+renderView1.CameraFocalPoint = [25.0983059214126, -7.58028478306764, -7.56438553653127]
+renderView1.CameraViewUp = [-0.09535621980896813, 0.9350079031724332, -0.34156611709716406]
+renderView1.CameraParallelScale = 81.0879152525208
+renderView1.Background = [1.0, 1.0, 1.0]
+renderView1.Background2 = [0.0, 0.0, 0.164705882352941]
 
-a3_uvw_PiecewiseFunction = CreatePiecewiseFunction( Points=[0.0, 0.0, 0.5, 0.0, 0.301629293155924, 1.0, 0.5, 0.0] )
+# ----------------------------------------------------------------
+# setup the data processing pipelines
+# ----------------------------------------------------------------
 
-a1_sigma_PVLookupTable = GetLookupTableForArray( "sigma", 1, RGBPoints=[0.0, 0.0, 0.0, 1.0, 480.0, 1.0, 0.0, 0.0], VectorMode='Magnitude', NanColor=[0.498039, 0.498039, 0.498039], ScalarOpacityFunction=a1_sigma_PiecewiseFunction, ColorSpace='HSV', ScalarRangeInitialized=1.0, LockScalarRange=1 )
+# create a new 'Legacy VTK Reader'
+tensiletestvtk = LegacyVTKReader(FileNames=['/home/gtheler/codigos/wasora-suite-0.5/fino/examples/tensile-test.vtk'])
 
-a3_uvw_PVLookupTable = GetLookupTableForArray( "u-v-w", 3, RGBPoints=[0.0, 0.23, 0.299, 0.754, 0.150814646577962, 0.865, 0.865, 0.865, 0.301629293155924, 0.706, 0.016, 0.15], VectorMode='Magnitude', NanColor=[0.25, 0.0, 0.0], ScalarOpacityFunction=a3_uvw_PiecewiseFunction, ColorSpace='Diverging', ScalarRangeInitialized=1.0 )
+# create a new 'Warp By Vector'
+warpByVector1 = WarpByVector(Input=tensiletestvtk)
+warpByVector1.Vectors = ['POINTS', 'u-v-w']
+warpByVector1.ScaleFactor = 50.0
 
-RenderView1 = CreateRenderView()
-RenderView1.CameraViewUp = [-0.09535621980896813, 0.9350079031724332, -0.34156611709716406]
-RenderView1.CacheKey = 0.0
-RenderView1.StereoType = 0
-RenderView1.UseLight = 1
-RenderView1.StereoRender = 0
-RenderView1.CameraPosition = [119.855276809304, 83.5687295595565, 215.494572652346]
-RenderView1.LightSwitch = 0
-RenderView1.OrientationAxesVisibility = 0
-RenderView1.Background2 = [0.0, 0.0, 0.164705882352941]
-RenderView1.CameraClippingRange = [176.44641413211806, 357.12471223423637]
-RenderView1.StereoCapableWindow = 0
-RenderView1.Background = [1.0, 1.0, 1.0]
-RenderView1.CameraFocalPoint = [25.0983059214126, -7.58028478306764, -7.56438553653127]
-RenderView1.CenterAxesVisibility = 0
-RenderView1.CameraParallelScale = 81.0879152525208
+# ----------------------------------------------------------------
+# setup color maps and opacity mapes used in the visualization
+# note: the Get..() functions create a new object, if needed
+# ----------------------------------------------------------------
 
-ScalarBarWidgetRepresentation1 = CreateScalarBar( Title='sigma', Enabled=1, LabelFontSize=6, LabelColor=[0.0, 0.0, 0.0], LookupTable=a1_sigma_PVLookupTable, TitleFontSize=8, TitleColor=[0.0, 0.0, 0.0], TitleFontFamily='Times' )
-GetRenderView().Representations.append(ScalarBarWidgetRepresentation1)
+# get color transfer function/color map for 'sigma'
+sigmaLUT = GetColorTransferFunction('sigma')
+sigmaLUT.LockDataRange = 1
+sigmaLUT.RGBPoints = [0.0, 0.0, 0.0, 0.5625, 66.6666, 0.0, 0.0, 1.0, 219.0477, 0.0, 1.0, 1.0, 295.2381, 0.5, 1.0, 0.5, 371.4285, 1.0, 1.0, 0.0, 523.8095999999999, 1.0, 0.0, 0.0, 600.0, 0.5, 0.0, 0.0]
+sigmaLUT.ColorSpace = 'RGB'
+sigmaLUT.NanColor = [0.498039215686, 0.498039215686, 0.498039215686]
+sigmaLUT.ScalarRangeInitialized = 1.0
 
-tensiletest_vtk = LegacyVTKReader( guiName="tensile-test.vtk", FileNames=['/home/gtheler/codigos/wasora-suite-0.5/fino/examples/tensile-test.vtk'] )
+# get opacity transfer function/opacity map for 'sigma'
+sigmaPWF = GetOpacityTransferFunction('sigma')
+sigmaPWF.Points = [0.0, 0.0, 0.5, 0.0, 600.0, 1.0, 0.5, 0.0]
+sigmaPWF.ScalarRangeInitialized = 1
 
-WarpByVector1 = WarpByVector( guiName="WarpByVector1", Vectors=['POINTS', 'u-v-w'], ScaleFactor=50.0 )
+# get color transfer function/color map for 'u-v-w'
+uvwLUT = GetColorTransferFunction('u-v-w')
+uvwLUT.RGBPoints = [0.0, 0.23, 0.299, 0.754, 0.150814646577962, 0.865, 0.865, 0.865, 0.301629293155924, 0.706, 0.016, 0.15]
+uvwLUT.NanColor = [0.25, 0.0, 0.0]
+uvwLUT.ScalarRangeInitialized = 1.0
 
-SetActiveSource(tensiletest_vtk)
-DataRepresentation1 = Show()
-DataRepresentation1.EdgeColor = [0.0, 0.0, 0.5000076295109483]
-DataRepresentation1.SelectionPointFieldDataArrayName = 'sigma'
-DataRepresentation1.ScalarOpacityFunction = a1_sigma_PiecewiseFunction
-DataRepresentation1.ColorArrayName = ('POINT_DATA', 'sigma')
-DataRepresentation1.ScalarOpacityUnitDistance = 10.910678224057
-DataRepresentation1.LookupTable = a1_sigma_PVLookupTable
-DataRepresentation1.ScaleFactor = 16.0
+# get opacity transfer function/opacity map for 'u-v-w'
+uvwPWF = GetOpacityTransferFunction('u-v-w')
+uvwPWF.Points = [0.0, 0.0, 0.5, 0.0, 0.301629293155924, 1.0, 0.5, 0.0]
 
-SetActiveSource(WarpByVector1)
-DataRepresentation2 = Show()
-DataRepresentation2.EdgeColor = [0.0, 0.0, 0.5000076295109483]
-DataRepresentation2.SelectionPointFieldDataArrayName = 'sigma'
-DataRepresentation2.ScalarOpacityFunction = a3_uvw_PiecewiseFunction
-DataRepresentation2.ScalarOpacityUnitDistance = 10.930702054107
-DataRepresentation2.AmbientColor = [0.0, 0.0, 0.0]
-DataRepresentation2.LookupTable = a3_uvw_PVLookupTable
-DataRepresentation2.Representation = 'Wireframe'
-DataRepresentation2.ScaleFactor = 16.0301352
+# ----------------------------------------------------------------
+# setup the visualization in view 'renderView1'
+# ----------------------------------------------------------------
 
-GetRenderView().ViewSize = [1200,800]
-Render()
+# show data from tensiletestvtk
+tensiletestvtkDisplay = Show(tensiletestvtk, renderView1)
+# trace defaults for the display properties.
+tensiletestvtkDisplay.ColorArrayName = ['POINTS', 'sigma']
+tensiletestvtkDisplay.LookupTable = sigmaLUT
+tensiletestvtkDisplay.Specular = 0.1
+tensiletestvtkDisplay.OSPRayScaleFunction = 'PiecewiseFunction'
+tensiletestvtkDisplay.GlyphType = 'Arrow'
+tensiletestvtkDisplay.SelectionPointLabelColor = [0.5, 0.5, 0.5]
+tensiletestvtkDisplay.ScalarOpacityUnitDistance = 10.910678224057
 
-WriteImage("tensile-test.png")
+# show color legend
+tensiletestvtkDisplay.SetScalarBarVisibility(renderView1, True)
 
+# show data from warpByVector1
+warpByVector1Display = Show(warpByVector1, renderView1)
+# trace defaults for the display properties.
+warpByVector1Display.Representation = 'Wireframe'
+warpByVector1Display.AmbientColor = [0.0, 0.0, 0.0]
+warpByVector1Display.ColorArrayName = ['POINTS', '']
+warpByVector1Display.LookupTable = uvwLUT
+warpByVector1Display.Specular = 0.1
+warpByVector1Display.OSPRayScaleFunction = 'PiecewiseFunction'
+warpByVector1Display.GlyphType = 'Arrow'
+warpByVector1Display.SelectionPointLabelColor = [0.5, 0.5, 0.5]
+warpByVector1Display.ScalarOpacityUnitDistance = 10.930702054107
+
+# setup the color legend parameters for each legend in this view
+
+# get color legend/bar for sigmaLUT in view renderView1
+sigmaLUTColorBar = GetScalarBar(sigmaLUT, renderView1)
+sigmaLUTColorBar.Position = [0.87, 0.25]
+sigmaLUTColorBar.Position2 = [0.13, 0.5]
+sigmaLUTColorBar.Title = 'Von Mises'
+sigmaLUTColorBar.ComponentTitle = '[MPa]'
+sigmaLUTColorBar.TitleColor = [0.0, 0.0, 0.0]
+sigmaLUTColorBar.TitleFontFamily = 'Times'
+sigmaLUTColorBar.TitleFontSize = 8
+sigmaLUTColorBar.LabelColor = [0.0, 0.0, 0.0]
+sigmaLUTColorBar.LabelFontSize = 6
+sigmaLUTColorBar.AutomaticLabelFormat = 0
+sigmaLUTColorBar.LabelFormat = '%g'
+sigmaLUTColorBar.RangeLabelFormat = '%g'
+
+# ----------------------------------------------------------------
+# finally, restore active source
+SetActiveSource(tensiletestvtk)
+# ----------------------------------------------------------------
