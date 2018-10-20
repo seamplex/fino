7fd1f6e176cfa9d12be6d3be87f5de1ecfe2ceaf
gtheler <jeremy@seamplex.com>
2018-10-20 16:08:17 -0300

    cantilever convergence example

--- examples/test-cantilever.sh
+++ examples/test-cantilever.sh
@@ -6,17 +6,24 @@
 checkgmsh
 checkm4
 
+if [ ! -z "$1" ]; then
+  n_max=$1
+else
+  n_max=5
+fi
+
+
 outcome=0
 for order in 1 2; do
  for struct in 0 1; do
-  runfino cantilever.fin $order $struct | tee cantilever-${order}-${struct}.dat
+  runfino cantilever.fin $order $struct $n_max | tee cantilever-${order}-${struct}.dat
   if [ $? -ne 0 ]; then
     outcome=99
   fi
  done
 done
 
-plot "set xrange [0:0.5]; set key bottom left; plot 'cantilever-1-0.dat' u 1:5 w lp, 'cantilever-1-1.dat' u 1:5 w lp, 'cantilever-2-0.dat' u 1:5 w lp, 'cantilever-2-1.dat' u 1:5 w lp"
+# plot "set xrange [1e-5:1e-1]; set logscale x; set key bottom left; plot 0.25, 'cantilever-1-0.dat' u 1:5 w lp, 'cantilever-1-1.dat' u 1:5 w lp, 'cantilever-2-0.dat' u 1:5 w lp, 'cantilever-2-1.dat' u 1:5 w lp"
 
 # exit
 exit $outcome
