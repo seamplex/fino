46c5f638767b0010de43a980b108e79f32b15fd4
gtheler <jeremy@seamplex.com>
2020-02-03 15:57:49 -0300

    one object for each dofs in reactions

--- src/reactions.c
+++ src/reactions.c
@@ -30,10 +30,10 @@ int fino_instruction_reaction(void *arg) {
   element_t *element;
   double R[3] = {0, 0, 0};
 
-  IS set_rows;
   IS set_cols;
-  Mat K_row;
-  Vec K_row_u;
+  IS set_rows[3];
+  Mat K_row[3];
+  Vec K_row_u[3];
   
   PetscInt i, j, j_local, g, k;
   PetscInt add;
@@ -51,6 +51,7 @@ int fino_instruction_reaction(void *arg) {
   k = 0;
   for (j = fino.first_node; j < fino.last_node; j++) {
     add = 0;
+    // esto se podria hacer barriendo los assigned_elements y viendo si pertenece a la entidad
     for (i = 0; add == 0 && i < reaction->physical_entity->n_elements; i++) {
       element = &fino.mesh->element[reaction->physical_entity->element[i]];
       for (j_local = 0; add == 0 && j_local < element->type->nodes; j_local++) {
@@ -70,21 +71,21 @@ int fino_instruction_reaction(void *arg) {
 
   for (g = 0; g < fino.degrees; g++) {
     // el de las filas depende del dofs
-    petsc_call(ISCreateGeneral(PETSC_COMM_WORLD, k, rows[g], PETSC_USE_POINTER, &set_rows));
-    petsc_call(MatCreateSubMatrix(fino.K_nobc, set_rows, set_cols, (g==0)?MAT_INITIAL_MATRIX:MAT_REUSE_MATRIX, &K_row));
-    if (g == 0) {
-      petsc_call(MatCreateVecs(K_row, PETSC_NULL, &K_row_u));
-    }  
-    petsc_call(MatMult(K_row, fino.phi, K_row_u));
-    petsc_call(VecSum(K_row_u, &R[g]));
-    petsc_call(ISDestroy(&set_rows));
+    petsc_call(ISCreateGeneral(PETSC_COMM_WORLD, k, rows[g], PETSC_USE_POINTER, &set_rows[g]));
+    // intente hacer un solo K_row y reusarlo para los G dofs pero daba mal en paralelo
+    petsc_call(MatCreateSubMatrix(fino.K_nobc, set_rows[g], set_cols, MAT_INITIAL_MATRIX, &K_row[g]));
+    petsc_call(MatCreateVecs(K_row[g], PETSC_NULL, &K_row_u[g]));
+    petsc_call(MatMult(K_row[g], fino.phi, K_row_u[g]));
+    petsc_call(VecSum(K_row_u[g], &R[g]));
+    gsl_vector_set(reaction->vector->value, g, R[g]);
+    petsc_call(VecDestroy(&K_row_u[g]));
+    petsc_call(MatDestroy(&K_row[g]));
+    petsc_call(ISDestroy(&set_rows[g]));
     free(rows[g]);
   }  
+  petsc_call(ISDestroy(&set_cols));
   free(rows);
     
-  petsc_call(VecDestroy(&K_row_u));
-  petsc_call(MatDestroy(&K_row));
-  petsc_call(ISDestroy(&set_cols));
 
   
   if (fino.problem_kind == problem_kind_axisymmetric) {
@@ -95,11 +96,7 @@ int fino_instruction_reaction(void *arg) {
       gsl_vector_set(reaction->vector->value, 0, 2*M_PI*R[0]);
       gsl_vector_set(reaction->vector->value, 1, 0);
     }
-  } else {  
-    for (g = 0; g < fino.degrees; g++) {
-      gsl_vector_set(reaction->vector->value, g, R[g]);
-    }
   }
-
+  
   return WASORA_RUNTIME_OK;
 }
